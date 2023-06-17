ea726487361a1e0386df4dae2c3d0e3f53854990
rbarbazz <42906704+rbarbazz@users.noreply.github.com>
2023-06-17 14:29:20 -0700

    feat: cache next prompts

--- src/getDailyPrompt.js
+++ src/getDailyPrompt.js
@@ -1,10 +1,49 @@
 const { mkdirSync, readFileSync, writeFileSync } = require('fs')
 const path = require('path')
 
-const prompts = readFileSync(path.join(__dirname, 'prompts.txt'), 'utf8')
-  .split('\n')
-  .filter(Boolean)
-const randomPrompt = prompts[Math.floor(Math.random() * prompts.length)]
+const NEXT_PROMPTS_FILE_PATH = 'next-prompts.txt'
+
+/**
+ * @param {string} pathToFile
+ * @returns {string[]}
+ */
+const getPromptsFromFile = (pathToFile) => {
+  let prompts = []
+
+  try {
+    prompts = readFileSync(path.join(__dirname, pathToFile), 'utf8')
+      .split('\n')
+      .filter(Boolean)
+  } catch (error) {
+    // File doesn't exist
+  }
+
+  return prompts
+}
+
+// Load the remaining prompts
+let nextPrompts = getPromptsFromFile(NEXT_PROMPTS_FILE_PATH)
+
+// If no more prompts, load the initial list
+if (!nextPrompts.length) {
+  const initialPrompts = getPromptsFromFile('initial-prompts.txt')
+
+  nextPrompts = [...initialPrompts]
+}
+
+// Select a prompt
+const randomPromptIndex = Math.floor(Math.random() * nextPrompts.length)
+const randomPrompt = nextPrompts[randomPromptIndex]
+
+// Update the next prompts list
+const updatedNextPrompts = [...nextPrompts]
+
+updatedNextPrompts.splice(randomPromptIndex, 1)
+writeFileSync(
+  path.join(__dirname, NEXT_PROMPTS_FILE_PATH),
+  updatedNextPrompts.join('\n'),
+  'utf8',
+)
 
 const htmlString = readFileSync(path.join(__dirname, 'index.html'), 'utf8')
 const modifiedHtmlString = htmlString.replace(
